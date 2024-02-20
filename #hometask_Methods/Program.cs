4788ebd8b9024fc650e865b1cf2f78aa397ae99d
nihadaslanli <nihadsasl@code.edu.az>
2024-02-21 00:42:33 +0400

    Add project files.

---
+++ #hometask_Methods/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+#region task1
+ 
+    int n =2;
+    int m = 6;
+    int sum = 0;
+    if (n < m)
+    {
+        sum += (m /= n);
+        return;
+    }
+
+Console.WriteLine(sum);
+
+#endregion
+
+#region task2
+
+
+
+#endregion
+
+#region task3
+//int n = 5;
+//int m = 23;
+//int count = 0;
+//for (int i = n; i < m; i++)
+//{if (i %2==1)
+//    {
+//        count++;
+//    }
+
+//}
+//Console.WriteLine(count);
+#endregion
+#region task4
+//int = 15;
+//if
+
+#endregion
+
+#region task5
+//int n = 16;
+
+//if(n%2==0)
+//{
+//    while (n /= 1) 
+
+//    { 
+//        Console.WriteLine("2nin quvvetidir");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("qüvvəti deyil");
+//}
+#endregion
